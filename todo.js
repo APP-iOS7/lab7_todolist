37f80b59f977bc06a1074a758f55a582d39671e9
bhyn <bhyn9785@gmail.com>
2024-12-27 14:30:54 +0900

    add filter

--- todo.js
+++ todo.js
@@ -1,6 +1,7 @@
 const addButton = document.getElementById("addTodo");
 const todoListElement = document.getElementById("todoList");
 const todoInput = document.getElementById("todoInput");
+const filterMenu = document.getElementById("filterMenu");
 
 function addTodo(text, checked = false) {
     // li 요소 만들기
@@ -28,15 +29,48 @@ function loadTodos() {
 // localStorage에 할일 목록 저장하기
 function saveTodos(todos) {
     localStorage.setItem("todoList", JSON.stringify(todos));
+    renderTodos();
+}
+
+// 검색 기능
+function filterTodos(filterType) {
+    let filteredTodos = [];
+  
+    const todos = loadTodos();
+    switch (filterType) {
+      case "completed":
+        filteredTodos = todos.filter(todo => todo.checked);
+        console.log("completed");
+        break;
+      case "doing":
+        filteredTodos = todos.filter(todo => !todo.checked);
+        console.log("doing");
+
+        break;
+      case "all":
+        console.log("all");
+
+      default:
+        filteredTodos = loadTodos();
+    }
+  
+    console.log("filterTodos", filteredTodos);
+    renderTodos(filteredTodos);
+  }
+
+
+// 할일을 렌더링하는 함수
+function renderTodos(todosToRender = loadTodos()) {
+    todoListElement.innerHTML = ""; // 기존 목록 비우기
+
+    todosToRender.forEach((todo) => {
+        addTodo(todo.text, todo.checked); // 필터링된 항목들을 화면에 추가
+    });
 }
 
 // 초기화 함수
 function initialize() {
-    // 저장된 할일 목록 불러오기
-    const todos = loadTodos();
-    todos.forEach((todo) => {
-        addTodo(todo.text, todo.checked);
-    });
+    renderTodos();
 
     // 새로운 할일 추가 버튼 클릭 이벤트
     addButton.addEventListener("click", () => {
@@ -57,6 +91,21 @@ function initialize() {
         // 입력창 비우기
         todoInput.value = "";
     });
+
+    filterMenu.addEventListener("click", (e) => {
+        console.log("data filtered")
+        const filterType = e.target.getAttribute("data-filter");
+        console.log("Filter Type:", filterType); // filterType 출력
+
+        if (filterType) {
+
+          filterTodos(filterType);
+          console.log("test")
+        }
+        else {
+            console.log("not filterType");
+        }
+      });
 }
 
 // 페이지 로드시 초기화
